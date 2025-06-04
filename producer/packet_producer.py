0e2bff712e19fd59aa113b162e4f6d3547945c7f
younginshin115 <younginshin115@gmail.com>
2025-06-04 22:11:52 +0900

    feat: Enhance packet processing in Kafka producer by integrating advanced packet parsing and adding features extraction

--- producer/packet_producer.py
+++ producer/packet_producer.py
@@ -1,8 +1,13 @@
 # producer/packet_producer.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 import pyshark
 import json
 from kafka import KafkaProducer
+from data.parser import parse_packet
 
 def create_kafka_producer(bootstrap_servers='localhost:9092'):
     return KafkaProducer(
@@ -31,13 +36,19 @@ def run_packet_stream_to_kafka(source: str, topic: str = 'packets', interface_mo
     for packet in capture.sniff_continuously():
         try:
             if hasattr(packet, 'ip'):
+                pf = parse_packet(packet)
+                if pf is None:
+                    continue  # skip non-IPv4 or malformed packets
+
                 payload = {
                     "timestamp": str(packet.sniff_time),
-                    "src_ip": packet.ip.src if hasattr(packet, 'ip') else None,
-                    "dst_ip": packet.ip.dst if hasattr(packet, 'ip') else None,
-                    "protocol": packet.transport_layer if hasattr(packet, 'transport_layer') else "Unknown",
-                    "length": int(packet.length)
+                    "src_ip": pf.id_fwd[0],
+                    "dst_ip": pf.id_fwd[2],
+                    "protocol": pf.id_fwd[4],
+                    "length": int(packet.length),
+                    "features": pf.features_list
                 }
+
                 producer.send(topic, payload)
                 print(f"[KAFKA SENT] {payload}")
         except Exception as e:
